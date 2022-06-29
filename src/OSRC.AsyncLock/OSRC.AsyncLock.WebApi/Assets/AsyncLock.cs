7dbf9b3b051cb5fee0bf352c5a77b1853b2e4bd0
A man has no name <crucio.is.awesome@windowslive.com>
2022-06-29 20:56:58 +0200

    feat(Api): code added
    monsters are living inside the code

---
+++ src/OSRC.AsyncLock/OSRC.AsyncLock.WebApi/Assets/AsyncLock.cs
@@ -0,0 +1,44 @@
+﻿namespace OSRC.AsyncLock.WebApi.Assets
+{
+    public class AsyncLock
+    {
+        private class Releaser : IDisposable
+        {
+            private bool disposedValue;
+
+            private SemaphoreSlim Locker { get; }
+
+            public Releaser(SemaphoreSlim locker)
+            {
+                Locker = locker;
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposedValue)
+                {
+                    if (disposing)
+                    {
+                        Console.WriteLine("Releasing locker");
+                        Locker.Release();
+                    }
+                    disposedValue = true;
+                }
+            }
+
+            public void Dispose()
+            {
+                Dispose(disposing: true);
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        private SemaphoreSlim AsyncLocker { get; } = new SemaphoreSlim(1, 1);
+
+        public async Task<IDisposable> AwaitAsync(CancellationToken cancellationToken)
+        {
+            await AsyncLocker.WaitAsync(cancellationToken);
+            return new Releaser(AsyncLocker);
+        }
+    }
+}
